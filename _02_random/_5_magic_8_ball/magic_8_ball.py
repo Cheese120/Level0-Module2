db22f3b74c52243a06fc431b5d2710c2b2d63687
league <catfish5701@gmail.com>
2024-08-09 11:57:19 -0700

    p

--- _02_random/_5_magic_8_ball/magic_8_ball.py
+++ _02_random/_5_magic_8_ball/magic_8_ball.py
@@ -6,17 +6,21 @@ if __name__ == '__main__':
     window.withdraw()
 
     # TODO Get the user to enter a question for the 8 ball to answer
-
+simpledialog.askstring(title='8-ball',prompt="ask me a yes or no question ad I'll answer it.")
     # Make a variable and initialize it to a random number between 0 and 3
-
+random_number = random.randint(0, 3)
     # If the random number is 0
-
+if random_number==0:
+    messagebox.showinfo(message="No")
         # tell the user "Yes"
-
+if random_number==1:
+    messagebox.showinfo(message="Yes")
     # If the random number is 1
-
+if random_number==2:
+    messagebox.showinfo(message="Maybe you should ask Google?")
         # tell the user "No"
-
+if random_number==3:
+    messagebox.showinfo(message="💀")
     # If the random number is 2
 
         # tell the user "Maybe you should ask Google?"
